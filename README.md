40d6362aa9f471bb21e081e0b4ecd44312f79212
hossein-nemati <80382370+hossein-nemati@users.noreply.github.com>
2021-11-27 20:26:25 +0330

    add catalog api data class

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # AspnetMicroservices
 AspnetMicroservices
+
+
+#run 
+docker pull mongo  # in solution directory
+
+#run
+docker run -d -p 27017:27017 --name shopping-mongo mongo  # for create shopping-mongo container
+
+#check it 
+docker ps # show running items
+
+#run
+docker exec -it shopping-mongo /bin/bash   # open a bash command line to mongodb container # for run mongodb command and access to mongodb 
+   => then in opened bash run mongo command to access mongodb 
---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreconfigurationProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreconfigurationProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Id = "id1",
+                    Name = "IPhone",
+                    Summary = "Summary",
+                    Description = "Description",
+                    ImageFile = "ImageFile",
+                    Price = 2323.0M,
+                    Categoy = "Categoy"
+                },
+                new Product()
+                {
+                    Id = "id2",
+                    Name = "Samsung",
+                    Summary = "Summary",
+                    Description = "Description",
+                    ImageFile = "ImageFile",
+                    Price = 3456.0M,
+                    Categoy = "Categoy"
+                }
+            };
+        }
+    }
+}
