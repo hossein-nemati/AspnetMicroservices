4084bcd74c155fcf68dc127b699fc4a5b384bfbf
hossein-nemati <80382370+hossein-nemati@users.noreply.github.com>
2022-01-02 06:55:21 +0330

    exeption and validation in application behaviours

---
+++ src/Services/Ordering/Ordering.Application/Behaviours/UnhandledExceptionBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Ordering.Application.Behaviours
+{
+    public class UnhandledExceptionBehaviour<TReqquest, TResponse> : IPipelineBehavior<TReqquest, TResponse>
+    {
+        private readonly ILogger<TReqquest> _logger;
+
+        public UnhandledExceptionBehaviour(ILogger<TReqquest> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<TResponse> Handle(TReqquest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                var requestName = typeof(TReqquest).Name;
+                _logger.LogError(ex, "Application Request: Unhandled Exception for Request {Name} {@Request}", requestName, request);
+                throw;
+            }
+        }
+    }
+}
