3f85200868c9ea279ad61da14b4d5cd435addb18
hossein-nemati <80382370+hossein-nemati@users.noreply.github.com>
2022-01-07 12:45:40 +0330

    apply ef migrations to sql server automatically in order api

--- src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -15,6 +15,7 @@ namespace Ordering.Infrastructure.Persistence
             {
                 orderContext.Orders.AddRange(GetPreconfiguredOrders());
                 await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
             }
         }
 
